aa2b618d87723b61ae32e7b2c04f0a0c75236130
mattkrebs1974 <mattkrebsemail@gmail.com>
2021-02-09 17:11:41 -0500

    switched to firebase

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import AppFirebase from './AppFirebase';
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppFirebase />, document.getElementById('root'));
