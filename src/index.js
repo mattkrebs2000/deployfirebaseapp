5c31200bb98788a2a0d6147c94506677255788d8
mattkrebs1974 <mattkrebsemail@gmail.com>
2021-02-09 17:07:43 -0500

    deployed

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppFirebase from './AppFirebase';
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById('root'));
